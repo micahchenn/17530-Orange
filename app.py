45438b26c1fd1297ef973003e0ef6c294f1950bc
micahchenn <micahchen@utexas.edu>
2024-11-14 17:42:42 -0600

    a

--- app.py
+++ app.py
@@ -27,10 +27,6 @@ app.config['SESSION_COOKIE_SECURE'] = True  # Use True if using HTTPS
 app.config['SERVER_NAME'] = "app-orange-hardware-b474fc6fdc47.herokuapp.com"
 #app.config['SERVER_NAME'] = 'localhost:5000'
 
-@app.route('/')
-def home():
-    return "Hello, World!"
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     # Extract data from request
